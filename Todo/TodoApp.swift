dc1c577f7afb1278984f7bca76cda41a223454e0
Ki2lian <kignon12@gmail.com>
2022-02-09 15:21:38 +0100

    Initial Commit

---
+++ Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by Killian on 09/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
